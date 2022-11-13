2d6e7f477bec14a0be7ccae8a62af5dda9c4d913
Arnaud Birk <arnaud.birk@ign.fr>
2022-11-13 22:03:11 +0100

    add question6

---
+++ Corrections/question6/Cube.java
@@ -0,0 +1,29 @@
+package question6;
+
+public class Cube extends Carre implements Scalable{
+	
+	private int z;
+	
+	public Cube(int x, int y, int z, double cote) {
+		super(x, y, cote);
+		this.z = z;
+	}
+	
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+",z="+this.z;
+	}
+	
+	@Override
+	public double surface() {
+		return super.surface()*cote;
+	}
+}
